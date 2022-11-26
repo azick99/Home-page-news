f010aac624ee2eec71e0320ebf635192c0d612ec
azick99 <yunusaliyevazizbek80@gmail.com>
2022-11-26 05:43:16 -0800

    first commit of news home page

---
+++ javaScript.js
@@ -0,0 +1,9 @@
+function w3_open() {
+    document.getElementById('navbarNavDropdown').style.display = 'block'
+    document.getElementById('myOverlay').style.display = 'block'
+  }
+  
+  function w3_close() {
+    document.getElementById('navbarNavDropdown').style.display = 'none'
+    document.getElementById('myOverlay').style.display = 'none'
+  }
